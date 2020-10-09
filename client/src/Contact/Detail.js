4d84ef869fdd9553c54b8ef24bd6c830d7c1b031
junko911 <junkotahara911@gmail.com>
2020-10-09 18:11:52 -0400

    Render tag component on detail page

--- client/src/Contact/Detail.js
+++ client/src/Contact/Detail.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { Row, Col } from 'reactstrap'
+import Tag from './Tag'
 
 const Detail = ({ contact }) => {
   return (
     <>
-      {contact ? <>
-        <h1>Contact Details</h1>
-        <h4>Name: {contact.name}</h4>
-        <h4>Status: {contact.status}</h4>
-        <h4>Created At: {contact.created_at}</h4>
-      </> : null}
+      {contact ?
+        <>
+          <Row>
+            <Col xs="8">
+              <h1>Contact Details</h1>
+              <h4>Name: {contact.name}</h4>
+              <h4>Status: {contact.status}</h4>
+              <h4>Created At: {contact.created_at}</h4>
+            </Col>
+            <Col xs="4">
+              <Tag />
+            </Col>
+          </Row>
+        </>
+        : null}
     </>
   )
 }
---
+++ client/src/Contact/Tag.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tag = () => {
+  return (
+    <h1>Tag</h1>
+  )
+}
+
+export default Tag
