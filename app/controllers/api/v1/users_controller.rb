8dbb240fd362005c67514e8dd6126a3d2c05ca4d
junko911 <junkotahara911@gmail.com>
2020-10-11 21:35:48 -0400

    Fix error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    byebug
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token(user_id: @user.id)
