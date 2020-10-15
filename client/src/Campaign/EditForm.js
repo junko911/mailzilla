d42b7cc563aba8338bc0d079074bb12aa8420a63
junko911 <junkotahara911@gmail.com>
2020-10-14 21:43:35 -0400

    Set ekeditor

--- client/src/Campaign/EditForm.js
+++ client/src/Campaign/EditForm.js
@@ -1,71 +1,50 @@
-import React from 'react'
-import { Row, Col, Button, Form, FormGroup } from 'reactstrap'
-import { Editor } from 'react-draft-wysiwyg'
-import { EditorState, convertToRaw, ContentState } from 'draft-js'
-import draftToHtml from 'draftjs-to-html'
-import htmlToDraft from 'html-to-draftjs'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import { updateCampaign } from '../redux/actions'
-import CKEditor from '@ckeditor/ckeditor5-react';
-
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-// import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-
-const editorConfiguration = {
-  // plugins: [Alignment],
-  toolbar: [ 'heading', '|', 'bulletedList', 'numberedList', 'alignment', 'undo', 'redo' ]
-};
+import React from "react";
+import { Row, Col, Button, Form, FormGroup } from "reactstrap";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw, ContentState } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import htmlToDraft from "html-to-draftjs";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { updateCampaign } from "../redux/actions";
+import CKEditor from "ckeditor4-react";
 
 class EditForm extends React.Component {
-
   state = {
     editorState: EditorState.createEmpty(),
-    content: ""
-  }
+  };
 
-  onEditorStateChange = (editorState) => {
-    this.setState({
-      editorState,
-    }, () => {
-      this.setState({ content: draftToHtml(convertToRaw(editorState.getCurrentContent())) })
+  submitHandler = (e) => {
+    e.preventDefault();
+    const id = this.props.campaign.id;
+    this.props.submitHandler(id, this.state.content).then(() => {
+      this.props.history.push(`/campaigns/${id}`);
     });
   };
 
-  submitHandler = e => {
-    e.preventDefault()
-    const id = this.props.campaign.id
-    this.props.submitHandler(id, this.state.content).then(() => {
-      this.props.history.push(`/campaigns/${id}`)
-    })
-  }
-
   componentDidMount() {
-    const contentBlock = htmlToDraft(this.props.campaign.content)
-    const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks)
-    const editorState = EditorState.createWithContent(contentState)
-    this.setState({ editorState, content: draftToHtml(convertToRaw(editorState.getCurrentContent())) })
-  }
-
-  uploadImageCallBack = (file) => {
-    return new Promise(
-      (resolve, reject) => {
-        const xhr = new XMLHttpRequest()
-        const fd = new FormData()
-        const token = localStorage.getItem("token")
-        xhr.open("POST", `http://localhost:3000/api/v1/campaigns/${this.props.campaign.id}/upload`)
-        xhr.setRequestHeader("Authorization", `Bearer ${token}`)
-        fd.append('file', file)
-        xhr.send(fd)
-        xhr.addEventListener('load', (data) => {
-          const response = JSON.parse(data.target.response)
-          resolve(response)
-        })
-      }
-    )
+    const contentBlock = htmlToDraft(this.props.campaign.content);
+    const contentState = ContentState.createFromBlockArray(
+      contentBlock.contentBlocks
+    );
+    const editorState = EditorState.createWithContent(contentState);
+    this.setState({
+      editorState,
+      content: draftToHtml(convertToRaw(editorState.getCurrentContent())),
+    });
   }
 
   render() {
+    const editorConfiguration = {
+      alignment: {
+        options: ["left", "right"],
+      },
+      toolbar: "Basic",
+      filebrowserImageUploadUrl: `http://localhost:3000/api/v1/campaigns/${this.props.campaign.id}/upload`,
+    };
+
+    CKEditor.editorUrl = "https://cdn.ckeditor.com/4.15.0/full/ckeditor.js";
+
     return (
       <>
         <h4>Campaign: {this.props.campaign.name}</h4>
@@ -76,54 +55,37 @@ class EditForm extends React.Component {
           <Row>
             <Col xs="9">
               <FormGroup>
-                <div style={{ minHeight: "500px", border: "1px solid #ced4da" }}>
-                  <CKEditor
-                    editor={ClassicEditor}
-                    // config={ editorConfiguration }
-                    data="<p>Hello from CKEditor 5!</p>"
-                    onInit={editor => {
-                      // You can store the "editor" and use when it is needed.
-                      console.log('Editor is ready to use!', editor);
-                    }}
-                    onChange={(event, editor) => {
-                      const data = editor.getData();
-                      console.log({ event, editor, data });
-                    }}
-                    onBlur={(event, editor) => {
-                      console.log('Blur.', editor);
-                    }}
-                    onFocus={(event, editor) => {
-                      console.log('Focus.', editor);
-                    }}
-                  />
-                  {/* <Editor
-                    editorState={this.state.editorState}
-                    wrapperClassName="wrapper-class"
-                    editorClassName="editor-class"
-                    onEditorStateChange={this.onEditorStateChange}
-                    toolbar={{
-                      image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true }, previewImage: true }
-                    }}
-                  /> */}
-                </div>
+                <CKEditor
+                  data={this.props.campaign.content}
+                  config={editorConfiguration}
+                  onChange={(event) => {
+                    this.setState({
+                      content: event.editor.getData(),
+                    });
+                  }}
+                />
               </FormGroup>
             </Col>
             <Col xs="3">
-              <Button color="primary" className="redirect-btn" >Save</Button>
+              <Button color="primary" className="redirect-btn">
+                Save
+              </Button>
             </Col>
           </Row>
         </Form>
       </>
-    )
+    );
   }
 }
 
-const msp = state => {
-  return { redirectTo: state.redirectTo }
-}
+const msp = (state) => {
+  return { redirectTo: state.redirectTo };
+};
 
-const mdp = dispatch => {
-  return { submitHandler: (id, content) => dispatch(updateCampaign(id, content)) }
-}
+const mdp = (dispatch) => {
+  return {
+    submitHandler: (id, content) => dispatch(updateCampaign(id, content)),
+  };
+};
 
-export default withRouter(connect(msp, mdp)(EditForm))
+export default withRouter(connect(msp, mdp)(EditForm));
--- client/src/Campaign/Preview.js
+++ client/src/Campaign/Preview.js
@@ -1,106 +1,128 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import { Row, Col, Button, Modal, ModalBody, ModalFooter } from 'reactstrap'
-import { sendToSegment } from '../redux/actions'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Row, Col, Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+import { sendToSegment } from "../redux/actions";
 
-const Preview = props => {
-  let foundCampaign = props.campaigns.find(campaign => campaign.id === props.id)
+const Preview = (props) => {
+  let foundCampaign = props.campaigns.find(
+    (campaign) => campaign.id === props.id
+  );
 
-  const [modal, setModal] = useState(false)
-  const toggle = () => setModal(!modal)
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
 
   const sendTest = () => {
-    const token = localStorage.getItem("token")
+    const token = localStorage.getItem("token");
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accepts': 'application/json',
-        Authorization: `Bearer ${token}`
-      }
-    }
-    fetch(`http://localhost:3000/api/v1/campaigns/${props.id}/send_test`, options)
-      .then(() => {
-        props.toggleAlert()
-      })
-  }
+        "Content-Type": "application/json",
+        Accepts: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    fetch(
+      `http://localhost:3000/api/v1/campaigns/${props.id}/send_test`,
+      options
+    ).then(() => {
+      props.toggleAlert();
+    });
+  };
 
   const sendToSegment = () => {
     props.sendToSegment(props.id).then(() => {
-      toggle()
-      props.toggleAlert()
-    })
-  }
+      toggle();
+      props.toggleAlert();
+    });
+  };
 
   return (
     <>
-      {
-        foundCampaign ?
+      {foundCampaign ? (
+        <>
+          <h1>Preview</h1>
           <>
-            <h1>Preview</h1>
-            <>
-              <h4>Subject: {foundCampaign.subject}</h4>
-              <Row>
-                <Col xs="9">
-                  {
-                    foundCampaign.content ?
-                      <div
-                        dangerouslySetInnerHTML={{ __html: foundCampaign.content }}
-                        style={{ border: "1px solid #ced4da", minHeight: "500px" }}
-                      ></div>
-                      : <div style={{ border: "1px solid #ced4da", minHeight: "500px" }}>No content</div>
-                  }
-                </Col>
-                <Col xs="3">
-                  <div>
-                    <Button
-                      color="secondary"
-                      className="redirect-btn"
-                      href={`/campaigns`}
-                    >Go back to campaigns</Button>
-                    <Button
-                      color="success"
-                      className="redirect-btn"
-                      href={`/campaigns/${foundCampaign.id}/edit`}
-                    >Edit</Button>
-                    <Button
-                      color="primary"
-                      onClick={sendTest}
-                      className="redirect-btn"
-                    >Send test to myself</Button>
-                    <Button
-                      color="danger"
-                      className="redirect-btn"
-                      onClick={toggle}
-                    >Send to segment</Button>
+            <h4>Subject: {foundCampaign.subject}</h4>
+            <Row>
+              <Col xs="9">
+                {foundCampaign.content ? (
+                  <div
+                    dangerouslySetInnerHTML={{ __html: foundCampaign.content }}
+                    style={{ border: "1px solid #ced4da", minHeight: "500px" }}
+                  ></div>
+                ) : (
+                  <div
+                    style={{ border: "1px solid #ced4da", minHeight: "500px" }}
+                  >
+                    No content
                   </div>
-                  <div>
-                    <Modal isOpen={modal} toggle={toggle}>
-                      <ModalBody>
-                        <h4>Are you sure?</h4>
-                        <ModalFooter>
-                          <Button color="primary" size="sm" onClick={sendToSegment}>Yes</Button>
-                          <Button color="secondary" size="sm" onClick={toggle}>Cancel</Button>
-                        </ModalFooter>
-                      </ModalBody>
-                    </Modal>
-                  </div>
-                </Col>
-              </Row>
-            </>
+                )}
+              </Col>
+              <Col xs="3">
+                <div>
+                  <Button
+                    color="secondary"
+                    className="redirect-btn"
+                    href={`/campaigns`}
+                  >
+                    Go back to campaigns
+                  </Button>
+                  <Button
+                    color="success"
+                    className="redirect-btn"
+                    href={`/campaigns/${foundCampaign.id}/edit`}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    color="primary"
+                    onClick={sendTest}
+                    className="redirect-btn"
+                  >
+                    Send test to myself
+                  </Button>
+                  <Button
+                    color="danger"
+                    className="redirect-btn"
+                    onClick={toggle}
+                  >
+                    Send to segment
+                  </Button>
+                </div>
+                <div>
+                  <Modal isOpen={modal} toggle={toggle}>
+                    <ModalBody>
+                      <h4>Are you sure?</h4>
+                      <ModalFooter>
+                        <Button
+                          color="primary"
+                          size="sm"
+                          onClick={sendToSegment}
+                        >
+                          Yes
+                        </Button>
+                        <Button color="secondary" size="sm" onClick={toggle}>
+                          Cancel
+                        </Button>
+                      </ModalFooter>
+                    </ModalBody>
+                  </Modal>
+                </div>
+              </Col>
+            </Row>
           </>
-          : null
-      }
+        </>
+      ) : null}
     </>
-  )
-}
+  );
+};
 
-const msp = state => {
-  return { campaigns: state.campaigns }
-}
+const msp = (state) => {
+  return { campaigns: state.campaigns };
+};
 
-const mdp = dispatch => {
-  return { sendToSegment: campaignId => dispatch(sendToSegment(campaignId)) }
-}
+const mdp = (dispatch) => {
+  return { sendToSegment: (campaignId) => dispatch(sendToSegment(campaignId)) };
+};
 
-export default connect(msp, mdp)(Preview)
+export default connect(msp, mdp)(Preview);
