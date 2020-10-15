6e1604f8f403ef2d9a1400c5bb147759d0a14873
junko911 <junkotahara911@gmail.com>
2020-10-15 16:17:44 -0400

    Display stats on campaign details page

--- client/src/Campaign/Details.js
+++ client/src/Campaign/Details.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Preview from './Preview'
-import { Alert } from 'reactstrap'
+import { Alert, Row, Col } from 'reactstrap'
 import moment from 'moment'
 
 const Details = ({ campaign }) => {
@@ -18,10 +18,21 @@ const Details = ({ campaign }) => {
             The campaign has been sent!
           </Alert>
           <h1>Campaign Details</h1>
-          <h4>Name: {campaign.name}</h4>
-          <h4>Segment: {campaign.segment.name}</h4>
-          <h4>Status: {campaign.status[0].toUpperCase() + campaign.status.slice(1)}</h4>
-          <h4>Created at: {moment(campaign.created_at).calendar()}</h4>
+          <Row>
+            <Col xs="6">
+              <h4>Name: {campaign.name}</h4>
+              <h4>Segment: {campaign.segment.name}</h4>
+              <h4>Status: {campaign.status[0].toUpperCase() + campaign.status.slice(1)}</h4>
+              <h4>Created at: {moment(campaign.created_at).calendar()}</h4>
+            </Col>
+            <Col>
+              <h4 xs="6">Stats:</h4>
+              <p>Sent: {campaign.sent}</p>
+              <p>Delivered: {campaign.delivered}</p>
+              <p>Open: {campaign.open}</p>
+              <p>Open rate: {campaign.open_rate * 100}%</p>
+            </Col>
+          </Row>
           <Preview id={campaign.id} toggleAlert={toggle} />
         </>
         : null}
