c48baeab6853039aa211a3e47665e312b4b7ed42
junko911 <junkotahara911@gmail.com>
2020-10-16 12:21:11 -0400

    Modify webhook action to update timestamp only for the first time

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -8,11 +8,11 @@ class Api::V1::WebhooksController < ApplicationController
       campaign_contact = CampaignContact.find(event["campaign_contact_id"])
       campaign_contact.update(status: event["event"])
       updated_at = campaign_contact.updated_at
-      if event["event"] == "delivered"
+      if event["event"] == "delivered" && campaign_contact.delivered_at == nil
         campaign_contact.update(delivered_at: updated_at)
-      elsif event["event"] == "open"
+      elsif event["event"] == "open" && campaign_contact.open_at == nil
         campaign_contact.update(open_at: updated_at)
-      elsif event["event"] == "click"
+      elsif event["event"] == "click" && campaign_contact.click_at == nil
         campaign_contact.update(click_at: updated_at)
       end
     end
