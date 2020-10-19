120d5d2a9bc040997e5b4ad45b9e201b037ea66f
junko911 <junkotahara911@gmail.com>
2020-10-19 15:23:06 -0400

    Add style to nav bar

--- client/src/Nav/Navbar.js
+++ client/src/Nav/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {
   Nav,
+  Collapse,
   Navbar,
   NavItem,
   NavLink,
@@ -17,34 +18,36 @@ const NavBar = props => {
         expand="md"
         style={{ height: "100px" }}
       >
-        <Nav horizontal="center" style={{ fontSize: "25px" }}>
-          <NavbarBrand href="/">
-              <img src={"./paper-plane.png"} width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy"/>
-                Mailzilla
-          </NavbarBrand>
-            {props.currentUser ?
-              <>
-                <NavItem>
-                  <NavLink href="/campaigns">Campaigns</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/contacts">Contacts</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink onClick={props.logoutHandler}>Logout</NavLink>
-                </NavItem>
-              </>
-              :
-              <>
-                <NavItem>
-                  <NavLink href="/login">Log In</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/signup">Sign Up</NavLink>
-                </NavItem>
-              </>
-            }
-        </Nav>
+        <NavbarBrand href="/" style={{ fontSize: "25px", paddingTop: "8px", paddingLeft: "100px", paddingRight: "50px" }}>
+          <img src={"./paper-plane.png"} width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />
+          <span>&nbsp;&nbsp;</span><strong>Mailzilla</strong>
+        </NavbarBrand>
+        {props.currentUser ?
+          <>
+            <Nav horizontal="center">
+              <NavItem>
+                <NavLink href="/campaigns">Campaigns</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/contacts">Contacts</NavLink>
+              </NavItem>
+            </Nav>
+            <Nav className="ml-auto auth">
+              <NavItem>
+                <NavLink onClick={props.logoutHandler}>Logout</NavLink>
+              </NavItem>
+            </Nav>
+          </>
+          :
+          <Nav className="ml-auto auth">
+            <NavItem>
+              <NavLink href="/login">Log In</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/signup">Sign Up</NavLink>
+            </NavItem>
+          </Nav>
+        }
       </Navbar>
     </div>
   )
