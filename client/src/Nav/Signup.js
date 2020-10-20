ceeb9f94f748117fc816bf78c753a34a638921c5
junko911 <junkotahara911@gmail.com>
2020-10-20 11:58:17 -0400

    Modify signup form

--- client/src/Nav/Signup.js
+++ client/src/Nav/Signup.js
@@ -29,52 +29,54 @@ class SignUp extends React.Component {
   render() {
     return (
       <div className="signup" style={{ width: "500px" }}>
-        <h1>Welcome to MailZilla</h1>
-        {this.state.errorMessages ?
-          <Alert color="danger">
-            <ul>
-              {this.state.errorMessages.map(msg => <li key={this.state.errorMessages.indexOf(msg)}>{msg}</li>)}
-            </ul>
-          </Alert>
-          : null
-        }
-        <div className="auth-msg">Already have an account? <a href="/login">Log in</a></div>
-        <div className="auth-form">
-          <AvForm onValidSubmit={this.submitHandler}>
-            <AvField
-              type="text"
-              name="name"
-              label="Name"
-              errorMessage="Please enter your name"
-              required
-              onChange={this.changeHandler}
-            />
-            <AvField
-              type="email"
-              name="email"
-              label="Email"
-              errorMessage="Please enter your email"
-              required
-              onChange={this.changeHandler}
-            />
-            <AvField
-              type="password"
-              name="password"
-              label="Password"
-              errorMessage="Please enter your password"
-              required
-              onChange={this.changeHandler}
-            />
-            <AvField
-              type="password"
-              name="password_confirmation"
-              label="Password Confirmation"
-              errorMessage="Please enter your password"
-              onChange={this.changeHandler}
-              required
-            />
-            <Button color="primary">Sign up</Button>
-          </AvForm>
+        <div className="main">
+          <h1>Welcome to MailZilla</h1>
+          {this.state.errorMessages ?
+            <Alert color="danger">
+              <ul>
+                {this.state.errorMessages.map(msg => <li key={this.state.errorMessages.indexOf(msg)}>{msg}</li>)}
+              </ul>
+            </Alert>
+            : null
+          }
+          <div className="auth-msg">Already have an account? <a href="/login">Log in</a></div>
+          <div className="auth-form">
+            <AvForm onValidSubmit={this.submitHandler}>
+              <AvField
+                type="text"
+                name="name"
+                label="Name"
+                errorMessage="Please enter your name"
+                required
+                onChange={this.changeHandler}
+              />
+              <AvField
+                type="email"
+                name="email"
+                label="Email"
+                errorMessage="Please enter your email"
+                required
+                onChange={this.changeHandler}
+              />
+              <AvField
+                type="password"
+                name="password"
+                label="Password"
+                errorMessage="Please enter your password"
+                required
+                onChange={this.changeHandler}
+              />
+              <AvField
+                type="password"
+                name="password_confirmation"
+                label="Password Confirmation"
+                errorMessage="Please enter your password"
+                onChange={this.changeHandler}
+                required
+              />
+              <Button color="primary">Sign up</Button>
+            </AvForm>
+          </div>
         </div>
       </div>
     )
