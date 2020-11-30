8260ad41864ea44a0b41266252029e7dec27b9b4
junko911 <junkotahara911@gmail.com>
2020-11-29 22:29:25 -0500

    Add go back button

--- client/src/Campaign/EditForm.js
+++ client/src/Campaign/EditForm.js
@@ -68,9 +68,9 @@ class EditForm extends React.Component {
             <Col xs="3">
               <Button color="success" className="redirect-btn">Save</Button>
             </Col>
-            {/* <Col xs="3">
-              <Button color="primary" className="redirect-btn" href={`/campaigns/${this.props.campaign.id}/preview`}>Preview</Button>
-            </Col> */}
+            <Col xs="3">
+              <Button color="secondary" className="redirect-btn" href={`/campaigns/${this.props.campaign.id}/preview`}>Go back to preview</Button>
+            </Col>
           </Row>
         </Form>
       </>
