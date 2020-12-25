abdc6d9a8a693f424c862faac6f0b85a3ba274a5
sasharevzin <sasha.nike@gmail.com>
2020-12-25 12:40:56 -0500

    Refactor campaign model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -15,12 +15,23 @@ class Campaign < ApplicationRecord
     delivered_rate = delivered / sent.to_f
     open = campaign_contacts.where.not(open_at: nil).count
     open_rate = open / delivered.to_f
-    bounce = campaign_contacts.where(status: "bounce").count + campaign_contacts.where(status: "dropped").count
+    bounce = campaign_contacts.where(status: %i(bounce dropped)).count
     bounce_rate = bounce / sent.to_f
     click = campaign_contacts.where.not(click_at: nil).count
     click_rate = click / delivered.to_f
     spamreport = campaign_contacts.where(status: "spamreport").count
     spamreport_rate = spamreport / delivered.to_f
+
+    contacts = campaign_contacts.map do |campaign_contact|
+      {
+        contact: campaign_contact.contact,
+        status: campaign_contact.status,
+        created_at: campaign_contact.created_at,
+        delivered_at: campaign_contact.delivered_at,
+        open_at: campaign_contact.open_at,
+        click_at: campaign_contact.click_at
+      }
+    end
 
     {
       id: id,
@@ -35,16 +46,7 @@ class Campaign < ApplicationRecord
       num_of_contacts: segment.contacts.length,
       template_id: template_id,
       user: user,
-      contacts: campaign_contacts.map { |campaign_contact|
-        {
-          contact: campaign_contact.contact,
-          status: campaign_contact.status,
-          created_at: campaign_contact.created_at,
-          delivered_at: campaign_contact.delivered_at,
-          open_at: campaign_contact.open_at,
-          click_at: campaign_contact.click_at,
-        }
-      },
+      contacts: contacts,
       sent: sent,
       delivered: delivered,
       delivered_rate: delivered_rate.round(4),
@@ -55,36 +57,37 @@ class Campaign < ApplicationRecord
       bounce: bounce,
       bounce_rate: bounce_rate.round(4),
       spamreport: spamreport,
-      spamreport_rate: spamreport_rate.round(4),
+      spamreport_rate: spamreport_rate.round(4)
     }
   end
 
   def send_test
-    fromAddress = SendGrid::Email.new(email: from)
+    from_address = SendGrid::Email.new(email: from)
     body = SendGrid::Content.new(type: "text/html", value: content)
-    sg = SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
     to = SendGrid::Email.new(email: user.email)
-    mail = SendGrid::Mail.new(fromAddress, subject, to, body)
+    mail = SendGrid::Mail.new(from_address, subject, to, body)
     sg.client.mail._("send").post(request_body: mail.to_json)
   end
 
   def send_to_segment
-    if status === "draft"
-      fromAddress = SendGrid::Email.new(email: from)
-      sg = SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
+    return unless draft?
 
-      segment.contacts.each { |contact|
-        value = Mustache.render(content, name: contact.name)
-        body = SendGrid::Content.new(type: "text/html", value: value)
-        campaign_contact = campaign_contacts.create(contact: contact)
-        to = SendGrid::Email.new(email: contact.email)
-        mail = SendGrid::Mail.new(fromAddress, subject, to, body)
-        mail.add_custom_arg(SendGrid::CustomArg.new(key: "campaign_contact_id", value: campaign_contact.id))
-        sg.client.mail._("send").post(request_body: mail.to_json)
-      }
+    from_address = SendGrid::Email.new(email: from)
 
-      update(status: :sent)
-      update(sent_at: Time.current)
+    segment.contacts.each do |contact|
+      value = Mustache.render(content, name: contact.name)
+      body = SendGrid::Content.new(type: "text/html", value: value)
+      campaign_contact = campaign_contacts.create(contact: contact)
+      to = SendGrid::Email.new(email: contact.email)
+      mail = SendGrid::Mail.new(from_address, subject, to, body)
+      mail.add_custom_arg(SendGrid::CustomArg.new(key: "campaign_contact_id", value: campaign_contact.id))
+      sg.client.mail._("send").post(request_body: mail.to_json)
     end
+
+    update(status: :sent, sent_at: Time.current)
+  end
+
+  def sg
+    @sg ||= SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
   end
 end
