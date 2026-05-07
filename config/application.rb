562c66279fed382a98e0df1bdec40b0bc7bf4b4f
Junko Tahara <junkotahara911@gmail.com>
2026-05-07 13:41:52 -0400

    Skip asset compilation for API-only app, fix Dotenv deprecation

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+Dotenv::Rails.load
 
 module Mod5FinalProjectBackend
   class Application < Rails::Application
